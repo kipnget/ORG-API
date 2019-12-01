acfa6cb4e847d6a26152d6f49e20ff56099e48b3
MUTAI <yegoallen@gmail.com>
2019-12-01 19:01:28 +0300

    this commit willset and get private values  for department

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -14,4 +14,50 @@ public class Department {
         this.total_employees = total_employees;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTotal_employees() {
+        return total_employees;
+    }
+
+    public void setTotal_employees(int total_employees) {
+        this.total_employees = total_employees;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department that = (Department) o;
+        return total_employees == that.total_employees &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, total_employees);
+    }
+}
